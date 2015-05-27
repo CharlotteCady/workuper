3de4ab8f4da7b9339158a36b1b82901322b55c4c
charlotte cady <charlotte.cady@hotmail.fr>
2015-05-27 15:14:30 +0200

    probleme avec la configuration de pundit et active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :users, only: [:show, :edit, :update]
+  resources :articles, :questions, :ressources do
+    resources :reviews, :votes, :favorites, only: [:index, :new, :create, :destroy]
+  end
+
   ActiveAdmin.routes(self)
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 end
